e59201d2250fc07b2b975ba7394c89fa79641efa
Katie Perry <katelynnperry99@gmail.com>
2023-11-30 17:38:50 +0000

    didn't work resetting old tailwind stuff

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 const Banner = () => {
   return (
-    <div className="flex items-center justify-center w-full h-96 bg-fixed bg-parallax bg-cover shadow-inner sm:bg-center"></div>
+    <div className="flex items-center justify-center h-96 bg-fixed bg-parallax bg-cover shadow-inner sm:bg-center"></div>
   );
 };
 
